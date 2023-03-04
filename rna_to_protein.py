09a4d3d0e90ca5f22237ed3fbcf10099d9a07877
scriptod911 <70215102+scriptod911@users.noreply.github.com>
2023-03-04 22:11:07 +0200

    Update rna_to_protein.py

--- rna_to_protein.py
+++ rna_to_protein.py
@@ -1,19 +1,5 @@
-import sys
 from Bio.Seq import Seq
-from Bio.Alphabet import generic_rna
-from Bio.Alphabet import generic_protein
 
 def rna_to_protein(rna_sequence):
-    # Create RNA sequence object
-    rna = Seq(rna_sequence, generic_rna)
-
-    # Translate RNA to protein using standard genetic code
-    protein = rna.translate(to_stop=False)
-
-    # Convert protein to string and return
-    return str(protein)
-
-if __name__ == "__main__":
-    rna_sequence = sys.argv[1]
-    protein_sequence = rna_to_protein(rna_sequence)
-    print(protein_sequence)
+    seq_obj = Seq(rna_sequence)
+    return seq_obj.translate()
